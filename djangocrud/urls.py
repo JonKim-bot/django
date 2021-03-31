665725bb80dcd4cd10bb94700a773296bcc43b31
boitan <yongrou74@hotmail.com>
2021-04-01 00:42:03 +0800

    python

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -20,5 +20,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('djangoroute/', include('firstdjango.urls')),
     path('blog/', include('blog.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 
 ]
