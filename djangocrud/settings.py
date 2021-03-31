ab9eb16bf1ab03518bad8cde7d1bf7a7c78e8a7c
boitan <yongrou74@hotmail.com>
2021-04-01 00:55:20 +0800

    save

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
